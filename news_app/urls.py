5a93185c54a547c9554096321e6b38eeba034fb7
unknown <Arabboydev>
2025-03-22 17:44:03 +0500

    Yangiliklar sayti

---
+++ news_app/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from .views import news_list, news_detail, HomaPageView, ContactPageView,page_404_View, about_pageView, \
+    LocalNewsView, TechnologyNewsView, SportNewsView, XorijNewsView
+
+urlpatterns = [
+    path('', HomaPageView.as_view(), name='home_page'),
+    path('news/', news_list, name="all_news_list"),
+    path('news/<slug:news>/', news_detail, name="news_detail_page"),
+    path('contact-us/', ContactPageView.as_view(), name='contact_page'),
+    path('404-page/', page_404_View, name='404_page'),
+    path('about_page/', about_pageView, name='about_page'),
+
+    path('local', LocalNewsView.as_view(), name="local_news_app"),
+    path('xorij', XorijNewsView.as_view(), name="xorij_news_app"),
+    path('technolgy', TechnologyNewsView.as_view(), name="texnalogiya_news_app"),
+    path('sport', SportNewsView.as_view(), name="sport_news_app"),
+]
